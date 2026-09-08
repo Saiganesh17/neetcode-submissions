55cb7c6cec85ce97d7476fe86ca59e2bd6ec2000
Sai Ganesh Reddy <109289610+Saiganesh17@users.noreply.github.com>
2026-09-08 14:11:18 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result=[]
+        def backtrack(start, subset):
+            result.append(subset.copy())
+            for i in range(start, len(nums)):
+                if i>start and nums[i]==nums[i-1]:
+                    continue
+                subset.append(nums[i])
+                backtrack(i+1, subset)
+                subset.pop()
+        backtrack(0,[])
+        return result
+        #Time and space complexity is O(N* 2^n) and O(n) respectively
